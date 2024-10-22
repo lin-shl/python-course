89eda20b5648112c77c3929e1a0801f6a097ad5d
lin-shl <126352406+lin-shl@users.noreply.github.com>
2024-10-22 09:12:01 +0300

    Update task_1.py
    Упустила из виду метод index.

--- Лабораторная_работа_1/task_1.py
+++ Лабораторная_работа_1/task_1.py
@@ -1,5 +1,6 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-numbers[4] = (sum((sum(numbers[:4]), sum(numbers[5:]))) / len(numbers))
-print("Измененный список:", numbers)
+wrong_index = numbers.index(None)
+numbers[wrong_index] = (sum((sum(numbers[:4]), sum(numbers[5:]))) / len(numbers))
 
+print("Измененный список:", numbers)
