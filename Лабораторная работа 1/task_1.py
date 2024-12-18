e49e8ce01c6bef8976e02b0c168ce2292fa1952a
lin-shl <126352406+lin-shl@users.noreply.github.com>
2024-12-18 17:00:37 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -2,76 +2,72 @@ import doctest
 
 
 class Auto:
-    """
-    Класс для представления автомобиля, зарегистрированного
-    при нарушении скоростного режима
-    """
-    def __init__(self, name: str, speed: float):
+    def __init__(self, mark, model, color: str):
         """
         Создание и подготовка к работе объекта "Автомобиль"
+        :param mark: Название марки автомобиля.
+        :param model: Название модели автомобиля.
+        :param color: Цвет автомобиля.
 
-        :param name: Название марки авто
-        :param speed: Скорость, при которой было зарегистрировано нарушение
+        Пример:
+        >>> auto = Auto("BMW", "3", "Красный")
+        >>> auto_mark = auto.mark
+        >>> print(auto_mark)
+        BMW
+        """
 
-        >>> auto = Auto("Honda Civic", 110)
-        >>> current_speed = auto.speed
-        >>> print(current_speed)
-        110
+        self.mark = mark
+        self.model = model
+        self.color = color
+
+        if not isinstance(mark, str):
+            raise TypeError("Марка должна быть типа str")
+        if not isinstance(model, str):
+            raise TypeError("Модель должна быть типа str")
+        if not isinstance(color, str):
+            raise TypeError("Цвет должен быть типа str")
+
+    def movement(self) -> bool:
+        """
+        Метод проверяет, движется ли машина
+        :return: Возвращает истину при движении.
+
+        Пример:
+        >>> auto = Auto("BMW", "3", "Красный")
+        >>> auto.movement()
+        """
+        ...
+
+
+    def speed_movement(self, time: int, distance: float) -> float:
+        """
+        Метод вычисляет скорость движения автомобиля исходя из заданных параметров расстояния и времени
+        :param time: Время, за которое автомобиль прошёл расстояние.
+        :param distance: Расстояние, которое прошёл автомобиль.
+        :return: Скорость, с которой автомобиль прошёл расстояние.
+
+        Пример:
+        >>> auto = Auto("BMW", "3", "Красный")
+        >>> auto.speed_movement(1266, 55.8)
+        """
+        if not isinstance(time, int):
+            raise TypeError("Время должно быть типа int")
+        if not isinstance(distance, (int, float)):
+            raise TypeError("Расстояние должно быть типа int или float")
+        ...
+
+    def start(self, speed: float) -> None:
+        """
+        Метод, выполняющий запуск автомобиля с определённой скоростью
+        :param speed: Скорость, с которой надо запустить автомобиль.
+
+        Пример:
+        >>> auto = Auto("BMW", "3", "Красный")
+        >>> auto.start(120)
         """
         if not isinstance(speed, (int, float)):
-            raise TypeError("Скорость должна быть типа int или float.")
-        if not isinstance(name, str):
-            raise TypeError("Название марки авто должно быть типа str.")
-        if speed < 0:
-            raise ValueError("Скорость не может быть отрицательной.")
-
-        self.name = name
-        self.speed = speed
-
-    def verdict(self, speed: float, normal_speed: float) -> bool:
-        """
-        Метод проверяет, есть ли нарушение, и выносит вердикт
-
-        :param normal_speed: Скорость, заданная для данного участка дороги
-        :param speed: Скорость, с которой ехал водитель
-        :return: Истина, если нарушение есть, Ложь – если нет
-
-        Пример:
-        >>> auto = Auto("Honda Civic", 110)
-        >>> current_speed = auto.speed
-        >>> auto.verdict(current_speed, 90)
-        True
-        >>> auto.verdict(current_speed, 120)
-        False
-        >>> auto.verdict(current_speed, 4)
-        Traceback (most recent call last):
-            ...
-        ValueError: Установленная скорость не может быть меньше 5 км/ч.
-        """
-        if not isinstance(normal_speed, (int, float)):
-            raise TypeError("Скорость должна быть типа int или float.")
-        if normal_speed < 5:
-            raise ValueError("Установленная скорость не может быть меньше 5 км/ч.")
-        return speed > normal_speed
-
-
-class Driver:
-    def __init__(self, name: str, surname: str):
-        """
-        Данные о водителе
-
-        :param name: Имя
-        :param surname: Фамилия
-
-        Пример:
-        >>> driver = Driver("Вася", "Пупкин")
-        >>> wrong_driver = Driver("Иван Иванов", 110)
-        Traceback (most recent call last):
-            ...
-        TypeError: Имя и фамилия должны быть типа str.
-        """
-        if not isinstance(name, str) or not isinstance(surname, str):
-            raise TypeError("Имя и фамилия должны быть типа str.")
+            raise TypeError("Скорость должна быть типа int или float")
+        ...
 
 
 class Fine:
@@ -79,31 +75,97 @@ class Fine:
         """
         Создание и подготовка штрафного листа
 
-        :param current_speed: Зафиксированная скорость
-        :param allowed_speed: Допустимая скорость
+        :param current_speed: Зафиксированная скорость.
+        :param allowed_speed: Допустимая скорость.
 
         Примеры:
-        >>> auto = Auto("Honda Civic", 110)
-        >>> speed = auto.speed
-        >>> fine = Fine(speed, 90)
+        >>> fine = Fine(150, 90)
         """
+
         self.current_speed = current_speed
         self.allowed_speed = allowed_speed
+
         if not isinstance(current_speed, (int, float)) or not isinstance(allowed_speed, (int, float)):
             raise TypeError("Скорость должна быть типа int или float.")
 
-    def fine(self) -> int:
+    def verdict(self) -> bool:
+        """
+        Метод проверяет, есть ли нарушение, и выносит вердикт
+
+        :return: Истина, если нарушение есть, Ложь – если нет.
+
+        Пример:
+        >>> fine = Fine(150, 90)
+        >>> fine.verdict()
+        """
+        ...
+
+    def value(self) -> int:
         """
         Функция, которая проверяет пороговое значение нарушения и возвращает
         соответствующий штраф
 
-        :return: Штраф в соответствии с пороговым значением
+        :return: Штраф в соответствии с пороговым значением.
 
         Пример:
-        >>> auto = Auto("Honda Civic", 110)
-        >>> speed = auto.speed
-        >>> fine = Fine(speed, 90)
-        >>> fine.fine()
+        >>> fine = Fine(150, 90)
+        >>> fine.value()
+        """
+        ...
+
+    def cancel(self) -> None:
+        """
+        Метод, отменяющий штраф
+
+        Пример:
+        >>> fine = Fine(80, 90)
+        >>> fine.cancel()
+        """
+        ...
+
+class Cam:
+    def __init__(self, memory: int):
+        """
+        Подготовка записывающего устройства
+
+        :param memory: Содержит значение объема хранилища устройства.
+
+        Пример:
+        >>> camera = Cam(128)
+        """
+        if not isinstance(memory, (int, float)):
+            raise TypeError("Объём памяти должен быть типа int или float")
+        if memory <= 0:
+            raise ValueError("Память не может быть отрицательно1 или равной нулю")
+
+    def record(self) -> None:
+        """
+        Метод, выполняющий запись в файл
+
+        Пример:
+        >>> camera = Cam(128)
+        >>> camera.record()
+        """
+        ...
+
+    def clear(self) -> None:
+        """
+        Метод, выполняющий очистку памяти
+
+        Пример:
+        >>> camera = Cam(128)
+        >>> camera.clear()
+        """
+        ...
+
+    def play(self):
+        """
+        Метод, выполняющий вывод записи на устройство вывода
+        :return: Запись с устройства.
+
+        Пример:
+        >>> camera = Cam(128)
+        >>> camera.play()
         """
         ...
 
